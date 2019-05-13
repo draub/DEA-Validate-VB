72b99455c412e8fb5b0334f89190220e00da7d73
Dan Raub <draub@draub.net>
2019-05-13 07:36:19 -0400

    Initial commit

---
+++ DEA Verify VB/frmDeaVerify.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDeaVerify
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtPrescriber = New System.Windows.Forms.TextBox()
+        Me.txtDea = New System.Windows.Forms.TextBox()
+        Me.cmdVerify = New System.Windows.Forms.Button()
+        Me.cmdClear = New System.Windows.Forms.Button()
+        Me.lblPrescriber = New System.Windows.Forms.Label()
+        Me.lblDea = New System.Windows.Forms.Label()
+        Me.lblResults = New System.Windows.Forms.Label()
+        Me.txtResults = New System.Windows.Forms.TextBox()
+        Me.lblResultsPass = New System.Windows.Forms.Label()
+        Me.lblResultsFail = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtPrescriber
+        '
+        Me.txtPrescriber.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPrescriber.Location = New System.Drawing.Point(23, 28)
+        Me.txtPrescriber.Name = "txtPrescriber"
+        Me.txtPrescriber.Size = New System.Drawing.Size(180, 22)
+        Me.txtPrescriber.TabIndex = 0
+        '
+        'txtDea
+        '
+        Me.txtDea.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDea.Location = New System.Drawing.Point(23, 85)
+        Me.txtDea.Name = "txtDea"
+        Me.txtDea.Size = New System.Drawing.Size(180, 22)
+        Me.txtDea.TabIndex = 1
+        '
+        'cmdVerify
+        '
+        Me.cmdVerify.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdVerify.Location = New System.Drawing.Point(23, 139)
+        Me.cmdVerify.Name = "cmdVerify"
+        Me.cmdVerify.Size = New System.Drawing.Size(75, 23)
+        Me.cmdVerify.TabIndex = 3
+        Me.cmdVerify.Text = "Verify"
+        Me.cmdVerify.UseVisualStyleBackColor = True
+        '
+        'cmdClear
+        '
+        Me.cmdClear.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdClear.Location = New System.Drawing.Point(128, 139)
+        Me.cmdClear.Name = "cmdClear"
+        Me.cmdClear.Size = New System.Drawing.Size(75, 23)
+        Me.cmdClear.TabIndex = 4
+        Me.cmdClear.Text = "Clear"
+        Me.cmdClear.UseVisualStyleBackColor = True
+        '
+        'lblPrescriber
+        '
+        Me.lblPrescriber.AutoSize = True
+        Me.lblPrescriber.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPrescriber.Location = New System.Drawing.Point(20, 12)
+        Me.lblPrescriber.Name = "lblPrescriber"
+        Me.lblPrescriber.Size = New System.Drawing.Size(134, 16)
+        Me.lblPrescriber.TabIndex = 5
+        Me.lblPrescriber.Text = "Prescriber Last Name"
+        '
+        'lblDea
+        '
+        Me.lblDea.AutoSize = True
+        Me.lblDea.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDea.Location = New System.Drawing.Point(20, 69)
+        Me.lblDea.Name = "lblDea"
+        Me.lblDea.Size = New System.Drawing.Size(83, 16)
+        Me.lblDea.TabIndex = 6
+        Me.lblDea.Text = "DEA Number"
+        '
+        'lblResults
+        '
+        Me.lblResults.AutoSize = True
+        Me.lblResults.Enabled = False
+        Me.lblResults.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResults.Location = New System.Drawing.Point(231, 12)
+        Me.lblResults.Name = "lblResults"
+        Me.lblResults.Size = New System.Drawing.Size(56, 16)
+        Me.lblResults.TabIndex = 7
+        Me.lblResults.Text = "Results:"
+        '
+        'txtResults
+        '
+        Me.txtResults.BackColor = System.Drawing.SystemColors.Control
+        Me.txtResults.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtResults.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtResults.Location = New System.Drawing.Point(234, 32)
+        Me.txtResults.Multiline = True
+        Me.txtResults.Name = "txtResults"
+        Me.txtResults.Size = New System.Drawing.Size(331, 145)
+        Me.txtResults.TabIndex = 8
+        Me.txtResults.TabStop = False
+        '
+        'lblResultsPass
+        '
+        Me.lblResultsPass.AutoSize = True
+        Me.lblResultsPass.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResultsPass.ForeColor = System.Drawing.Color.Green
+        Me.lblResultsPass.Location = New System.Drawing.Point(291, 10)
+        Me.lblResultsPass.Name = "lblResultsPass"
+        Me.lblResultsPass.Size = New System.Drawing.Size(52, 19)
+        Me.lblResultsPass.TabIndex = 9
+        Me.lblResultsPass.Text = "PASS"
+        Me.lblResultsPass.Visible = False
+        '
+        'lblResultsFail
+        '
+        Me.lblResultsFail.AutoSize = True
+        Me.lblResultsFail.Font = New System.Drawing.Font("Arial", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResultsFail.ForeColor = System.Drawing.Color.Red
+        Me.lblResultsFail.Location = New System.Drawing.Point(293, 10)
+        Me.lblResultsFail.Name = "lblResultsFail"
+        Me.lblResultsFail.Size = New System.Drawing.Size(43, 19)
+        Me.lblResultsFail.TabIndex = 10
+        Me.lblResultsFail.Text = "FAIL"
+        Me.lblResultsFail.Visible = False
+        '
+        'frmDeaVerify
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(593, 189)
+        Me.Controls.Add(Me.lblResultsFail)
+        Me.Controls.Add(Me.lblResultsPass)
+        Me.Controls.Add(Me.txtResults)
+        Me.Controls.Add(Me.lblResults)
+        Me.Controls.Add(Me.lblDea)
+        Me.Controls.Add(Me.lblPrescriber)
+        Me.Controls.Add(Me.cmdClear)
+        Me.Controls.Add(Me.cmdVerify)
+        Me.Controls.Add(Me.txtDea)
+        Me.Controls.Add(Me.txtPrescriber)
+        Me.Name = "frmDeaVerify"
+        Me.Text = "Verify DEA Number"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtPrescriber As TextBox
+    Friend WithEvents txtDea As TextBox
+    Friend WithEvents cmdVerify As Button
+    Friend WithEvents cmdClear As Button
+    Friend WithEvents lblPrescriber As Label
+    Friend WithEvents lblDea As Label
+    Friend WithEvents lblResults As Label
+    Friend WithEvents txtResults As TextBox
+    Friend WithEvents lblResultsPass As Label
+    Friend WithEvents lblResultsFail As Label
+End Class
---
+++ DEA Verify VB/frmDeaVerify.vb
@@ -0,0 +1,226 @@
+﻿Public Class frmDeaVerify
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles txtDea.TextChanged
+
+    End Sub
+
+    Private Sub CmdClear_Click(sender As Object, e As EventArgs) Handles cmdClear.Click
+        clearResults()
+        txtPrescriber.Clear()
+        txtDea.Clear()
+        txtPrescriber.BackColor = Color.White
+        txtDea.BackColor = Color.White
+    End Sub
+
+
+    Private Sub clearResults()
+        txtResults.Clear()
+        lblResults.Enabled = False
+        lblResultsPass.Visible = False
+        lblResultsFail.Visible = False
+    End Sub
+
+    Private Sub CmdVerify_Click(sender As Object, e As EventArgs) Handles cmdVerify.Click
+        Dim fieldsPopulated, deaCheckPass, deaGroupValid, deaNumeric As Boolean
+        Dim prescriber, dea, deaGroup, txtDeaCheckNotes As String
+        Dim deaNum, deaCheckSumA, deaCheckSumB, deaCheckSumTotal As Integer
+
+
+        'Clear any previous results
+        If txtResults.Text <> "" Then
+            clearResults()
+        End If
+
+        'Remove any spaces from input fields
+        prescriber = txtPrescriber.Text
+        dea = txtDea.Text
+
+        prescriber = prescriber.Replace(" ", "").ToUpper
+        dea = dea.Replace(" ", "").ToUpper
+
+        txtPrescriber.Text = prescriber
+        txtDea.Text = dea
+
+        'Verify that required fields are populated
+        fieldsPopulated = verifyFieldsPopulated(prescriber, dea)
+
+
+
+        If fieldsPopulated = True Then
+
+
+            'Set as if true.  Any failure of the checks will set to False
+            deaCheckPass = True
+
+            'Check 1:  Must be 9 characters long
+            If Len(dea) = 9 Then
+                txtDeaCheckNotes = "- Pass: DEA number is 9 characters long" & vbNewLine & vbNewLine
+            Else
+                txtDeaCheckNotes = "Fail: Incorrect number of characters" & vbNewLine & vbNewLine
+                deaCheckPass = False
+            End If
+
+            'Check 2.A:  First character must be a letter.  
+            If IsNumeric(dea(0)) = False Then
+
+                'Check 2.B:  Letters correspond to certain groups.  Group must be a valid group.
+                deaGroupValid = True
+
+                Select Case dea(0)
+                    Case "A"
+                        deaGroup = "A – Deprecated (used by some older entities)"
+                    Case "B"
+                        deaGroup = "B – Hospital/Clinic"
+                    Case "C"
+                        deaGroup = "C – Practitioner"
+                    Case "D"
+                        deaGroup = "D – Teaching Institution"
+                    Case "E"
+                        deaGroup = "E – Manufacturer"
+                    Case "F"
+                        deaGroup = "F – Distributor"
+                    Case "G"
+                        deaGroup = "G – Researcher"
+                    Case "H"
+                        deaGroup = "H – Analytical Lab"
+                    Case "J"
+                        deaGroup = "J – Importer"
+                    Case "K"
+                        deaGroup = "K – Exporter"
+                    Case "L"
+                        deaGroup = "L – Reverse Distributor"
+                    Case "M"
+                        deaGroup = "M – Mid Level Practitioner"
+                    Case "P", "R", "S", "T", "U"
+                        deaGroup = dea(0) & " – Narcotic Treatment Program"
+                    Case "R"
+                        deaGroup = "R – Narcotic Treatment Program"
+                    Case "X"
+                        deaGroup = "X – Suboxone/Subutex Prescribing Program"
+                    Case Else
+                        deaGroup = dea(0) & " - NOT LISTED AS ANY VALID GROUP"
+                        deaGroupValid = False
+                        deaCheckPass = False
+                End Select
+
+                If deaGroupValid = True Then
+                    txtDeaCheckNotes = txtDeaCheckNotes & "- Pass:  First character is a VALID group" & vbNewLine
+                    txtDeaCheckNotes = txtDeaCheckNotes & "        " & deaGroup & vbNewLine & vbNewLine
+                Else
+                    txtDeaCheckNotes = txtDeaCheckNotes & "- Fail:  First character is not a VALID group" & vbNewLine
+                    txtDeaCheckNotes = txtDeaCheckNotes & "        " & deaGroup & vbNewLine & vbNewLine
+                End If
+
+            Else
+                txtDeaCheckNotes = txtDeaCheckNotes & "- Fail:  First character is not a letter" & vbNewLine & vbNewLine
+                deaCheckPass = False
+
+            End If
+
+
+            'Check 3:  Second character must be either Prescriber's Last Name Initial, or in some cases it can be the number 9.
+            If dea(1) = prescriber(0) Then
+                txtDeaCheckNotes = txtDeaCheckNotes & "- Pass:  Second character matches Prescriber's initial" & vbNewLine & vbNewLine
+            ElseIf Val(dea(1)) = 9 Then
+                txtDeaCheckNotes = txtDeaCheckNotes & "- Pass:  Second character can be 9" & vbNewLine & vbNewLine
+            Else
+                txtDeaCheckNotes = txtDeaCheckNotes & "- Fail:  Second character mismatch" & vbNewLine & vbNewLine
+                deaCheckPass = False
+            End If
+
+            'Check 4:  Remaining characters must be numbers
+            deaNumeric = True
+            For deaNum = 3 To 8
+                If IsNumeric(dea(deaNum)) = False Then
+                    deaNumeric = False
+                End If
+            Next
+
+            If deaNumeric = True Then
+                txtDeaCheckNotes = txtDeaCheckNotes & "- Pass:  Remaining characters are numeric" & vbNewLine & vbNewLine
+            Else
+                txtDeaCheckNotes = txtDeaCheckNotes & "- Fail:  Remaining characters are not numeric" & vbNewLine & vbNewLine
+                deaCheckPass = False
+            End If
+
+
+            'Check 5:  Last character is the verfication check:  It needs to match the last digit of the following formula:
+            '(Characters 3 + 5 + 7) + ((Characters 4 + 6 + 8) * 2)
+            If deaNumeric = True Then
+                deaCheckSumA = Val(dea(2)) + Val(dea(4)) + Val(dea(6))
+                deaCheckSumB = (Val(dea(3)) + Val(dea(5)) + Val(dea(7))) * 2
+                deaCheckSumTotal = deaCheckSumA + deaCheckSumB
+
+                If Microsoft.VisualBasic.Right(dea, 1) = Microsoft.VisualBasic.Right(deaCheckSumTotal, 1) Then
+                    txtDeaCheckNotes = txtDeaCheckNotes & "- Pass:  Checksum calculation passes"
+                Else
+                    txtDeaCheckNotes = txtDeaCheckNotes & "- Fail:  Checksum calculation fails"
+                    deaCheckPass = False
+                End If
+
+            Else
+                txtDeaCheckNotes = txtDeaCheckNotes & "- Fail:  Unable to calculate checksum"
+                deaCheckPass = False
+            End If
+
+
+
+            lblResults.Enabled = True
+
+            If deaCheckPass = True Then
+                lblResultsPass.Visible = True
+            Else
+                lblResultsFail.Visible = True
+            End If
+
+            txtResults.Text = txtDeaCheckNotes
+
+
+        End If
+
+    End Sub
+
+
+
+    Function verifyFieldsPopulated(prescriber As String, dea As String) As Boolean
+        Dim msgText, msgTitle As String
+
+
+        If prescriber <> "" And dea <> "" Then
+            txtPrescriber.BackColor = Color.White
+            txtDea.BackColor = Color.White
+            Return True
+        Else
+            msgText = "Please input the following required fields:" & vbNewLine
+            msgTitle = "Missing fields"
+
+            If prescriber = "" Then
+                msgText = msgText & "- Prescriber Last Name" & vbNewLine
+                txtPrescriber.BackColor = Color.Yellow
+            Else
+                txtPrescriber.BackColor = Color.White
+            End If
+
+            If dea = "" Then
+                msgText = msgText & "- DEA Number"
+                txtDea.BackColor = Color.Yellow
+            Else
+                txtDea.BackColor = Color.White
+            End If
+
+            'Set focus to first missing text field.
+            If prescriber = "" Then
+                txtPrescriber.Select()
+            Else
+                txtDea.Select()
+            End If
+
+            MessageBox.Show(msgText, msgTitle, MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            Return False
+        End If
+    End Function
+
+    Private Sub LblResultsPass_Click(sender As Object, e As EventArgs) Handles lblResultsPass.Click
+
+    End Sub
+End Class
